a6375a4ebdeaebc64a9e8ea6ecdaaf7fa17f73d7
yanghs0416 <yanghs0416@naver.com>
2023-07-03 12:02:31 +0900

    first commit

---
+++ Triangle.java
@@ -0,0 +1,25 @@
+
+class Triangle {
+
+		public static void main(String[] args) {
+
+
+
+
+
+	System.out.println("     *"); 
+	
+
+
+	System.out.println("   **"); 
+
+	System.out.println("  ***"); 
+
+	System.out.println(" ****"); 
+
+	System.out.println("*****"); 
+
+
+		}
+
+}
---
+++ hello.java
@@ -0,0 +1,8 @@
+class HelloWord{
+		public static void main(String[] args{
+		System.println("Hello");	
+
+		}
+}
+
+
