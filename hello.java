b3a2cd3c4714f15916dfba29a8a2f07318eb5060
yanghs0416 <yanghs0416@naver.com>
2023-07-06 14:39:08 +0900

    Hello 수정

--- hello.java
+++ hello.java
@@ -1,6 +1,10 @@
 class HelloWord{
 		public static void main(String[] args{
 		System.println("Hello");	
+		System.println("Hello");	
+		System.println("Hello");	
+		System.println("Hello");	
+		System.println("Hello");	
 
 		}
 }
