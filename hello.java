7e0e18d46c97c0547e41bb4dbe23bdf0c40ea56e
yanghs0416 <yanghs0416@naver.com>
2023-07-06 17:12:15 +0900

    adfa

--- hello.java
+++ hello.java
@@ -1,10 +1,10 @@
 class HelloWord{
-		public static void main(String[] args{
-		System.println("Hello");	
-		System.println("Hello");	
-		System.println("Hello");	
-		System.println("Hello");	
-		System.println("Hello");	
+	public static void main(String[] args) {
+		System.out.println("Hello");	
+		System.out.println("Hello");	
+		System.out.println("Hello");	
+		System.out.println("Hello");	
+		System.out.println("Hello");	
 
 		}
 }
